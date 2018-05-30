62010570b8c15618164a3fe4b9e2027bd17bfa0b
Farid Rener <proteusvacuum@gmail.com>
2018-05-29 21:21:02 -0400

    Fix javascript tests

--- corehq/apps/reports/static/reports/js/filters/case_list_explorer.js
+++ corehq/apps/reports/static/reports/js/filters/case_list_explorer.js
@@ -1,4 +1,4 @@
-var SuggestedCaseTypes = function(){
+var suggestedCaseTypes = function(){
     // Adds the required properties to filter the case type autocomplete dropdowns
     var self = this;
     self.currentCaseType = ko.observable('');
@@ -16,7 +16,7 @@ var SuggestedCaseTypes = function(){
     });
 };
 
-hqDefine("reports/js/filters/case_properties", ['jQuery', 'underscore', 'knockout'], function($, _, ko) {
+hqDefine("reports/js/filters/case_list_explorer", ['jQuery', 'underscore', 'knockout'], function($, _, ko) {
     'use strict';
 
     var Property = function ($parent, name, label, editable, hidden) {
@@ -50,11 +50,11 @@ hqDefine("reports/js/filters/case_properties", ['jQuery', 'underscore', 'knockou
         return self;
     };
 
-    var CasePropertyColumnsViewModel = function(initialColumns, allCaseProperties) {
+    var casePropertyColumnsViewModel = function(initialColumns, allCaseProperties) {
         var self = this;
 
         self.allCaseProperties = allCaseProperties;
-        SuggestedCaseTypes.apply(self);
+        suggestedCaseTypes.apply(self);
 
         self.properties = ko.observableArray();
         for (var i = 0; i < initialColumns.length; i++){
@@ -76,21 +76,19 @@ hqDefine("reports/js/filters/case_properties", ['jQuery', 'underscore', 'knockou
 
         return self;
     };
-    return {model: CasePropertyColumnsViewModel};
-});
 
-
-hqDefine("reports/js/filters/case_search_xpath", function() {
-    var CaseSearchXpathViewModel = function(allCaseProperties){
+    var caseSearchXpathViewModel = function(allCaseProperties){
         var self = this;
         self.allCaseProperties = allCaseProperties;
-        SuggestedCaseTypes.apply(self);
+        suggestedCaseTypes.apply(self);
         return self;
     };
 
-    return {model: CaseSearchXpathViewModel};
+    return {
+        casePropertyColumns: casePropertyColumnsViewModel,
+        caseSearchXpath: caseSearchXpathViewModel,
+    };
 });
-
 
 ko.bindingHandlers.xPathAutocomplete = {
     init: function(element) {
